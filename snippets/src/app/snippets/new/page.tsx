d485491f4a507ada6c9434b3a222638535a224cc
Benjamin FABRY <fabrybenjamin5@gmail.com>
2024-02-21 20:25:28 +0100

    Section 3

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,6 +1,25 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formDate: FormData) {
+    "use server";
+
+    const title = formDate.get("title") as string;
+    const code = formDate.get("code") as string;
+
+    await db.snippet.create({
+      data: {
+        code,
+        title,
+      },
+    });
+
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
