13484f52f4ff778a758667471590a2ecf008aca9
Benjamin FABRY <fabrybenjamin5@gmail.com>
2024-03-13 23:23:30 +0100

    Section 7

---
+++ discuss/src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
+}
